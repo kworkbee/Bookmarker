ecad22144e7f3bdb7116cb3d53f12447fcab2fe5
G1 <g1.tommy.jeon@gmail.com>
2022-09-04 23:04:18 +0900

    Inital Commit

---
+++ src/main/java/me/g1tommy/bookmarker/BookmarkerApplication.java
@@ -0,0 +1,13 @@
+package me.g1tommy.bookmarker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmarkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookmarkerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/g1tommy/bookmarker/BookmarkerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.g1tommy.bookmarker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmarkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
